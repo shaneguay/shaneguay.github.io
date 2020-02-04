f5889254c59fb8e0cf869b1230acb93c33da72d0
Shane Guay <shane.guay@canopygrowth.com>
2020-02-04 14:05:31 -0500

    Initial commit

---
+++ precache-manifest.994501b01f5ee13720c840e6f7c027e7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccf21389e48ebfbe0f656aae7afee00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4360d975cee23d08339c",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7e0a314ab2023d478899",
+    "url": "/static/css/main.ce33f25c.chunk.css"
+  },
+  {
+    "revision": "4360d975cee23d08339c",
+    "url": "/static/js/2.70ddf1d0.chunk.js"
+  },
+  {
+    "revision": "8392ede6cadae463252ff2c08b5d3ce1",
+    "url": "/static/js/2.70ddf1d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e0a314ab2023d478899",
+    "url": "/static/js/main.7bd553ba.chunk.js"
+  },
+  {
+    "revision": "b7b15e04963421495769",
+    "url": "/static/js/runtime-main.1f5347da.js"
+  },
+  {
+    "revision": "7c0de86d6f16fbdfd5a427a13cb8fa16",
+    "url": "/static/media/Houndstooth.7c0de86d.png"
+  },
+  {
+    "revision": "0098f804fc2d06af52650e0b8ed3390c",
+    "url": "/static/media/Montserrat-Medium.0098f804.ttf"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/static/media/Montserrat-Regular.a8a11736.ttf"
+  },
+  {
+    "revision": "3c498b0a8545ac4bf9371eafa4cedc39",
+    "url": "/static/media/jujuNoNameBanner.3c498b0a.png"
+  },
+  {
+    "revision": "7f796742cddf8b5a083cefda5fe76d0f",
+    "url": "/static/media/juju_fern_white.7f796742.png"
+  },
+  {
+    "revision": "e900130ea45cb36aaa606ccacc704055",
+    "url": "/static/media/tweed_logo.e900130e.png"
+  }
+]);
